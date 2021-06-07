d6e17600b67e8d252d09c4a24ce96e9f54694535
lmp20 <leena.poikolainen20@imperial.ac.uk>
2021-06-07 10:40:46 +0100

    imported OZ contracts and made Bax Ownable removing the dependecy to Auth

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,24 +1,16 @@
 pragma solidity >=0.6.0;
 
 // Import Auth contract
-import "./access-control/Auth.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Box {
+contract Box is Ownable {
     uint256 private value;
-    Auth private auth;
 
     // Emitted when the stored value changes
     event ValueChanged(uint256 newValue);
 
-    constructor(Auth _auth) public {
-        auth = _auth;
-    }
-
-    // Sotres a new value in the contract
-    function store(uint256 newValue) public {
-        // Require that the caller is registered as an admin
-        require(auth.isAdministrator(msg.sender), "Unauthorized");
-        
+    // Stores a new value in the contract
+    function store(uint256 newValue) public onlyOwner {
         value = newValue;
         emit ValueChanged(newValue);
     }

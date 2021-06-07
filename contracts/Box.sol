61067c43160acaacda7ccd9ff27b723aff668855
lmp20 <leena.poikolainen20@imperial.ac.uk>
2021-06-07 10:35:22 +0100

    created Auth contract and addedd access logic

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,13 +1,24 @@
 pragma solidity >=0.6.0;
 
+// Import Auth contract
+import "./access-control/Auth.sol";
+
 contract Box {
     uint256 private value;
+    Auth private auth;
 
     // Emitted when the stored value changes
     event ValueChanged(uint256 newValue);
 
+    constructor(Auth _auth) public {
+        auth = _auth;
+    }
+
     // Sotres a new value in the contract
     function store(uint256 newValue) public {
+        // Require that the caller is registered as an admin
+        require(auth.isAdministrator(msg.sender), "Unauthorized");
+        
         value = newValue;
         emit ValueChanged(newValue);
     }

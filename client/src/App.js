53f9a378fa1895836aa702b905bc440ba09d81b1
lmp20 <leena.poikolainen20@imperial.ac.uk>
2021-08-17 09:57:29 +0100

    removed redundant lines from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { Component, useState } from 'react';
-import Web3 from 'web3'
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
@@ -53,7 +51,6 @@ import YellowIssuer from './pages/yellowIssuer';
 import YellowCompany from './pages/yellowCompany';
 import YellowRegulator from './pages/yellowRegulator';
 
-// import Navbar from './components/Navbar';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -124,43 +121,3 @@ class App extends Component {
 }
 
 export default App;
-{/* 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path='/' exact component={About} />
-        <Route path='/about' component={About} />
-        <Route path='/buybonds' component={BuyBonds} />
-      </Switch>
-    </Router> 
-  );
-}
-
-export default App;
-
-*/}
-
-
-{/* 
-    <>
-    <Navbar/>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    </>
-    */}

5470738ddf19655b12fece394029b0cefeb43887
lmp20 <leena.poikolainen20@imperial.ac.uk>
2021-08-20 08:01:31 +0100

    added a button option for bond navigation, on red

--- client/src/components/Red/pagination.js
+++ client/src/components/Red/pagination.js
@@ -2,12 +2,32 @@ import {
     Nav,
 } from 'react-bootstrap'
 
+import { Link } from 'react-router-dom';
+
 const Pagination = () => {
     return (
         <>
             
-            <div className="container mr-auto ml-auto mt-4 text-center  border">
+            <div className="container mr-auto ml-auto mt-4 text-center py-3 ">
                 <div className="font-weight-bold">Bond Red</div>
+                <div class="btn-group" role="group" aria-label="Basic example">
+				<button className="btn btn-light">
+					<Link to="/red" className="text-dark">Back Details</Link>
+				</button>
+				<button className="btn btn-light">
+					<Link to="/buyred" className="text-dark">Investor Page</Link>
+				</button>
+				<button className="btn btn-light">
+					<Link to="/issuerred" className="text-dark">Issuer Page</Link>
+				</button>
+				<button className="btn btn-light">
+					<Link to="/companyred" className="text-dark">Company Page</Link>
+				</button>
+				<button className="btn btn-light">
+					<Link to="/regulatorred" className="text-dark">Regulator Page</Link>
+				</button>
+			</div>
+            {/*    
             <Nav className="justify-content-center">
                 <Nav.Link href="/red"><button className="btn btn-light">BondDetails</button></Nav.Link>
                 <Nav.Link href="/buyred"><button className="btn btn-light">Investor Page</button></Nav.Link>
@@ -15,6 +35,7 @@ const Pagination = () => {
                 <Nav.Link href="/companyred"><button className="btn btn-light">Company Page</button></Nav.Link>
                 <Nav.Link href="/regulatorred"><button className="btn btn-light">Regulator Page</button></Nav.Link>
             </Nav>
+            */}
             </div>     
         </>
     );
--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,20 +1,14 @@
 import React, { Component, useState } from 'react';
-import Web3 from 'web3';
+import { Link } from 'react-router-dom';
 
 const About = () => {
 
-return (
-	<div
-	style={{
-		display: 'flex',
-		justifyContent: 'Right',
-		alignItems: 'Right',
-		height: '100vh'
-	}}
-	>
-	<h1>About the Platform</h1>
-	</div>
-);
+	return (
+		<div className="container-fluid my-5 px-5">
+			<h1>Platform for Green Bond Issuance</h1>
+			
+		</div>
+	);
 };
 
 export default About;
